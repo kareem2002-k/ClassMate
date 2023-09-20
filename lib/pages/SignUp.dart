babce81a1e853bbf49c170dedd0ba32cad3a9ce2
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-09-20 16:59:00 +0300

    validated the textfields

--- lib/pages/SignUp.dart
+++ lib/pages/SignUp.dart
@@ -11,6 +11,7 @@ class _SignUpState extends State<SignUp> {
   final nameCont = TextEditingController();
   final emailCont = TextEditingController();
   final passCont = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   var hidePass = true;
   var passIcons = const Icon(Icons.visibility_off);
 
@@ -23,184 +24,200 @@ class _SignUpState extends State<SignUp> {
         elevation: 0,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-              child: Text(
-                "Name",
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-              child: TextField(
-                controller: nameCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0)),
-                  hintText: 'Enter your name',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                child: Text(
+                  "Name",
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-              child: Text(
-                "E-mail",
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-              child: TextField(
-                controller: emailCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0)),
-                  hintText: 'Enter your e-mail',
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-              child: Text(
-                "Password",
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-              child: TextField(
-                obscureText: hidePass,
-                controller: passCont,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0)),
-                  hintText: 'Enter your password',
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      hidePass = !hidePass;
-                      passIcons = hidePass
-                          ? const Icon(Icons.visibility_off)
-                          : const Icon(Icons.visibility);
-                      setState(() {});
-                    },
-                    icon: passIcons,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                child: TextFormField(
+                  validator: (val) => val!.isEmpty ? 'Enter a Name' : null,
+                  controller: nameCont,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0)),
+                    hintText: 'Enter your name',
                   ),
                 ),
               ),
-            ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                child: Text(
+                  "E-mail",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                child: TextFormField(
+                  validator: (val) => val!.isEmpty ? 'Enter an E-mail' : null,
+                  controller: emailCont,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0)),
+                    hintText: 'Enter your e-mail',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                child: Text(
+                  "Password",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                child: TextFormField(
+                  validator: (val) => val!.length < 6
+                      ? 'Enter a password with length of 6+ chars'
+                      : null,
+                  obscureText: hidePass,
+                  controller: passCont,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0)),
+                    hintText: 'Enter your password',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        hidePass = !hidePass;
+                        passIcons = hidePass
+                            ? const Icon(Icons.visibility_off)
+                            : const Icon(Icons.visibility);
+                        setState(() {});
+                      },
+                      icon: passIcons,
+                    ),
+                  ),
+                ),
+              ),
 
-            const SizedBox(height: 50),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(12.0)),
-                  child: MaterialButton(
-                    onPressed: () {},
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Sign Up",
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        )
-                      ],
+              const SizedBox(height: 50),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(12.0)),
+                    child: MaterialButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print(emailCont.text);
+                          print(passCont.text);
+                        }
+                      },
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Sign Up",
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            //const SizedBox(height: 20.0),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
+              //const SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 8.0),
+                        height: 1.0, // Adjust the height as needed
+                        color: Colors.grey[600], // Color of the dashes
+                      ),
+                    ),
+                    Text(
+                      "or",
+                      style: TextStyle(
+                        color: Colors.grey[800],
+                        fontSize: 18,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 8.0),
+                        height: 1.0, // Adjust the height as needed
+                        color: Colors.grey[600], // Color of the dashes
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey.shade300, // Border color
+                          width: 2.0, // Border width
+                        ),
+                        borderRadius: BorderRadius.circular(12.0)),
+                    child: MaterialButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          print(emailCont.text);
+                          print(passCont.text);
+                        }
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 50.0,
+                            height: 50.0,
+                            // padding: EdgeInsets.all(10.0),
+                            alignment: Alignment.center,
+                            child: Image.asset("assets/images/google.png"),
+                          ),
+                          const Text(
+                            "Register with Google",
+                            style: TextStyle(fontSize: 18),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.only(right: 8.0),
-                      height: 1.0, // Adjust the height as needed
-                      color: Colors.grey[600], // Color of the dashes
-                    ),
-                  ),
                   Text(
-                    "or",
+                    "Already signed up ?",
                     style: TextStyle(
-                      color: Colors.grey[800],
-                      fontSize: 18,
+                      fontSize: 15,
+                      color: Colors.grey[600],
                     ),
                   ),
-                  Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.only(left: 8.0),
-                      height: 1.0, // Adjust the height as needed
-                      color: Colors.grey[600], // Color of the dashes
-                    ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Login"),
                   ),
                 ],
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey.shade300, // Border color
-                        width: 2.0, // Border width
-                      ),
-                      borderRadius: BorderRadius.circular(12.0)),
-                  child: MaterialButton(
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 50.0,
-                          height: 50.0,
-                          // padding: EdgeInsets.all(10.0),
-                          alignment: Alignment.center,
-                          child: Image.asset("assets/images/google.png"),
-                        ),
-                        const Text(
-                          "Register with Google",
-                          style: TextStyle(fontSize: 18),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already signed up ?",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey[600],
-                  ),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    await Navigator.pushNamed(context, '/login');
-                  },
-                  child: const Text("Login"),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
