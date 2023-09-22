68376dd852fdf3af48674595bd2041b703621cb4
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-09-22 20:09:44 +0300

    Update SettingsPage.dart

--- lib/pages/SettingsPage.dart
+++ lib/pages/SettingsPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +25,13 @@ class SettingsPage extends StatelessWidget {
                   decoration: BoxDecoration(
                     border: Border(
                       top: BorderSide(
-                          width: 1.0,
-                          color: Colors.grey.shade300), // Top border
+                        width: 1.0,
+                        color: Colors.grey.shade300,
+                      ), // Top border
                       bottom: BorderSide(
-                          width: 1.0,
-                          color: Colors.grey.shade300), // Bottom border
+                        width: 1.0,
+                        color: Colors.grey.shade300,
+                      ), // Bottom border
                     ),
                   ),
                   child: Row(
@@ -38,6 +40,7 @@ class SettingsPage extends StatelessWidget {
                       Icon(
                         Icons.person,
                         size: 50,
+                        color: Colors.grey[400],
                       ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -66,16 +69,102 @@ class SettingsPage extends StatelessWidget {
                         width: 100,
                       ),
                       CircleAvatar(
-                          backgroundColor: Colors.grey[350],
-                          child: IconButton(
-                            icon: Icon(Icons.logout),
-                            onPressed: () {},
-                          )),
+                        backgroundColor: Colors.grey[350],
+                        child: IconButton(
+                          icon: Icon(Icons.logout),
+                          onPressed: () {},
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              child: Expanded(
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(30, 0, 16, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // Profile Tab
+                      InkWell(
+                        onTap: () {
+                          // Handle the row tap action here
+                        },
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.grey[300],
+                              child: Icon(Icons.person_outline),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text("Profile"),
+                            SizedBox(
+                              width: 200,
+                            ),
+                            Icon(Icons.navigate_next),
+                          ],
+                        ),
+                      ),
+
+                      // Settings Tab
+                      InkWell(
+                        onTap: () {
+                          // Handle the row tap action here
+                        },
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.grey[300],
+                              child: Icon(Icons.settings),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text("Settings"),
+                            SizedBox(
+                              width: 200,
+                            ),
+                            Icon(Icons.navigate_next),
+                          ],
+                        ),
+                      ),
+                      // About Tab
+                      InkWell(
+                        onTap: () {
+                          // Handle the row tap action here
+                        },
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.grey[300],
+                              child: Icon(Icons.info),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text("About Us"),
+                            SizedBox(
+                              width: 200,
+                            ),
+                            Icon(Icons.navigate_next),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
           ],
         ),
       ),
