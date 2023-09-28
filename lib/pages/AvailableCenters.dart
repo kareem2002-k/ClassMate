56b38d54f9bf96bd792935342fe8c1367089554a
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-09-28 17:17:44 +0300

    Available Centers

---
+++ lib/pages/AvailableCenters.dart
@@ -0,0 +1,41 @@
+import 'package:class_mate/widgets/CenterItem.dart';
+import 'package:flutter/material.dart';
+
+class AvailableCenters extends StatefulWidget {
+  final String courseID;
+  const AvailableCenters({super.key, required this.courseID});
+
+  @override
+  State<AvailableCenters> createState() => _AvailableCentersState();
+}
+
+class _AvailableCentersState extends State<AvailableCenters> {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CenterItem(
+                centerID: "centerID",
+                centerName: "",
+                instructorName: "instructorName",
+                courseDate: "courseDate",
+                courseTime: "courseTime"),
+            CenterItem(
+                centerID: "centerID",
+                centerName: "",
+                instructorName: "instructorName",
+                courseDate: "courseDate",
+                courseTime: "courseTime")
+          ],
+        ),
+      ),
+    );
+  }
+}
