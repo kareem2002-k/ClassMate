35180f3622218d4fb098a30dc1a3ba48ee269ace
Mohamed-Fadel222 <116431641+Mohamed-Fadel222@users.noreply.github.com>
2023-09-28 17:04:38 +0300

    Center item Widget

--- lib/widgets/CenterItem.dart
+++ lib/widgets/CenterItem.dart
@@ -6,20 +6,78 @@ class CenterItem extends StatelessWidget {
   final String instructorName;
   final String courseDate;
   final String courseTime;
+
   const CenterItem({
-    required this.courseDate,
-    Key? key,
     required this.centerID,
     required this.centerName,
     required this.instructorName,
+    required this.courseDate,
     required this.courseTime,
+    Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 300,
+      height:300 ,
+
+      decoration: BoxDecoration(
+        color: Color(0xFFC2EBEE),
+        borderRadius: BorderRadius.circular(25.0),
+
+      ),
+
+
+
+      padding: const EdgeInsets.all(5.0),
       child: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Center: $centerName',
+            style:const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              color: Colors.black,
+              decoration: TextDecoration.none,
+            ),
+          ),
+
+          Text(
+            'Instructor: $instructorName',
+            textAlign: TextAlign.center,
+            style:const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.black,
+              decoration: TextDecoration.none,
+            ),
+          ),
+
+          Text(
+            'Course Date: $courseDate',
+            textAlign: TextAlign.center,
+            style:const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.black,
+              decoration: TextDecoration.none,
+            ),
+          ),
+
+           Text(
+            'Course Time: $courseTime',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.black,
+              decoration: TextDecoration.none,
+            ),
+          ),
+        ],
       ),
     );
   }
